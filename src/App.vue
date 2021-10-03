b5616aef0ca9c6891717f7b4792d53f075aab7fb
Mert Çalkan <mertcalkan@hotmail.com>
2021-10-03 15:20:55 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+        <inc-header></inc-header>
+        <router-view></router-view>
+        <loading></loading>
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from './components/includes/Header.vue';
+import Loading from './components/Loading.vue';
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+    components: {
+        incHeader: Header,
+        loading: Loading,
+    },
+    created() {
+        this.$store.dispatch('favoriteInit');
+    },
+};
+</script>
